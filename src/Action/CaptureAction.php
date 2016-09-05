0022bca0a0e633b00a723c2a6a23021516c15bfc
Recca Tsai <recca0120.gmail.com>
2016-09-05 12:12:37 +0800

    remove GatewayAwareAction

--- src/Action/CaptureAction.php
+++ src/Action/CaptureAction.php
@@ -2,22 +2,23 @@
 
 namespace PayumTW\Esunbank\Action;
 
-use Payum\Core\Action\GatewayAwareAction;
+use Payum\Core\Action\ActionInterface;
 use Payum\Core\ApiAwareInterface;
+use Payum\Core\ApiAwareTrait;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Exception\RequestNotSupportedException;
 use Payum\Core\Exception\UnsupportedApiException;
+use Payum\Core\GatewayAwareInterface;
+use Payum\Core\GatewayAwareTrait;
 use Payum\Core\Reply\HttpPostRedirect;
 use Payum\Core\Request\Capture;
 use Payum\Core\Request\GetHttpRequest;
 use PayumTW\Esunbank\Api;
 
-class CaptureAction extends GatewayAwareAction implements ApiAwareInterface
+class CaptureAction implements ActionInterface, ApiAwareInterface, GatewayAwareInterface
 {
-    /**
-     * @var Api
-     */
-    protected $api;
+    use ApiAwareTrait;
+    use GatewayAwareTrait;
 
     /**
      * {@inheritdoc}
